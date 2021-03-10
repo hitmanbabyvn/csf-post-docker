8ca51e3db8a61f21f7d1978b2791fe840f2f043a
hitmanbabyvn <32060207+hitmanbabyvn@users.noreply.github.com>
2021-03-10 15:48:37 +0700

    Update docker.sh
    add full path

--- docker.sh
+++ docker.sh
@@ -9,17 +9,17 @@ chain_exists() {
     }
     local chain_name="$1" ; shift
     [ $# -eq 1 ] && local table="--table $1"
-    iptables $table -n --list "$chain_name" >/dev/null 2>&1
+    /sbin/iptables $table -n --list "$chain_name" >/dev/null 2>&1
 }
 
 add_to_forward() {
     local docker_int=$1
 
-    if [ `iptables -nvL FORWARD | grep ${docker_int} | wc -l` -eq 0 ]; then
-        iptables -A FORWARD -o ${docker_int} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-        iptables -A FORWARD -o ${docker_int} -j DOCKER
-        iptables -A FORWARD -i ${docker_int} ! -o ${docker_int} -j ACCEPT
-        iptables -A FORWARD -i ${docker_int} -o ${docker_int} -j ACCEPT
+    if [ `/sbin/iptables -nvL FORWARD | grep ${docker_int} | wc -l` -eq 0 ]; then
+        /sbin/iptables -A FORWARD -o ${docker_int} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+        /sbin/iptables -A FORWARD -o ${docker_int} -j DOCKER
+        /sbin/iptables -A FORWARD -i ${docker_int} ! -o ${docker_int} -j ACCEPT
+        /sbin/iptables -A FORWARD -i ${docker_int} -o ${docker_int} -j ACCEPT
     fi
 }
 
@@ -27,38 +27,40 @@ add_to_nat() {
     local docker_int=$1
     local subnet=$2
 
-    iptables -t nat -A POSTROUTING -s ${subnet} ! -o ${docker_int} -j MASQUERADE
-    iptables -t nat -A DOCKER -i ${docker_int} -j RETURN
+    /sbin/iptables -t nat -A POSTROUTING -s ${subnet} ! -o ${docker_int} -j MASQUERADE
+    /sbin/iptables -t nat -A DOCKER -i ${docker_int} -j RETURN
 }
 
 add_to_docker_isolation() {
     local docker_int=$1
 
-    iptables -A DOCKER-ISOLATION-STAGE-1 -i ${docker_int} ! -o ${docker_int} -j DOCKER-ISOLATION-STAGE-2
-    iptables -A DOCKER-ISOLATION-STAGE-2 -o ${docker_int} -j DROP
+    /sbin/iptables -A DOCKER-ISOLATION-STAGE-1 -i ${docker_int} ! -o ${docker_int} -j DOCKER-ISOLATION-STAGE-2
+    /sbin/iptables -A DOCKER-ISOLATION-STAGE-2 -o ${docker_int} -j DROP
 }
 
 DOCKER_INT="docker0"
 DOCKER_NETWORK="172.17.0.0/16"
+DOCKER_NETWORK2="10.42.0.0/16"
 
 iptables-save | grep -v -- '-j DOCKER' | iptables-restore
-chain_exists DOCKER && iptables -X DOCKER
-chain_exists DOCKER nat && iptables -t nat -X DOCKER
+chain_exists DOCKER && /sbin/iptables -X DOCKER
+chain_exists DOCKER nat && /sbin/iptables -t nat -X DOCKER
 
-iptables -N DOCKER
-iptables -N DOCKER-ISOLATION-STAGE-1
-iptables -N DOCKER-ISOLATION-STAGE-2
-iptables -N DOCKER-USER
+/sbin/iptables -N DOCKER
+/sbin/iptables -N DOCKER-ISOLATION-STAGE-1
+/sbin/iptables -N DOCKER-ISOLATION-STAGE-2
+/sbin/iptables -N DOCKER-USER
 
-iptables -t nat -N DOCKER
+/sbin/iptables -t nat -N DOCKER
 
-iptables -A FORWARD -j DOCKER-USER
-iptables -A FORWARD -j DOCKER-ISOLATION-STAGE-1
+/sbin/iptables -A FORWARD -j DOCKER-USER
+/sbin/iptables -A FORWARD -j DOCKER-ISOLATION-STAGE-1
 add_to_forward ${DOCKER_INT}
 
-iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER
-iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER
-iptables -t nat -A POSTROUTING -s ${DOCKER_NETWORK} ! -o ${DOCKER_INT} -j MASQUERADE
+/sbin/iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER
+/sbin/iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER
+/sbin/iptables -t nat -A POSTROUTING -s ${DOCKER_NETWORK} ! -o ${DOCKER_INT} -j MASQUERADE
+/sbin/iptables -t nat -A POSTROUTING -s ${DOCKER_NETWORK2} ! -o ${DOCKER_INT} -j MASQUERADE
 
 bridges=`docker network ls -q --filter='Driver=bridge'`
 
@@ -98,15 +100,15 @@ if [ `echo ${containers} | wc -c` -gt "1" ]; then
                 dst_port=`echo ${dst} | awk -F'/' '{ print $1 }'`
                 dst_proto=`echo ${dst} | awk -F'/' '{ print $2 }'`
 
-                iptables -A DOCKER -d ${ipaddr}/32 ! -i ${DOCKER_NET_INT} -o ${DOCKER_NET_INT} -p ${dst_proto} -m ${dst_proto} --dport ${dst_port} -j ACCEPT
+                /sbin/iptables -A DOCKER -d ${ipaddr}/32 ! -i ${DOCKER_NET_INT} -o ${DOCKER_NET_INT} -p ${dst_proto} -m ${dst_proto} --dport ${dst_port} -j ACCEPT
 
-                iptables -t nat -A POSTROUTING -s ${ipaddr}/32 -d ${ipaddr}/32 -p ${dst_proto} -m ${dst_proto} --dport ${dst_port} -j MASQUERADE
+                /sbin/iptables -t nat -A POSTROUTING -s ${ipaddr}/32 -d ${ipaddr}/32 -p ${dst_proto} -m ${dst_proto} --dport ${dst_port} -j MASQUERADE
 
                 iptables_opt_src=""
                 if [ ${src_ip} != "0.0.0.0" ]; then
                     iptables_opt_src="-d ${src_ip}/32 "
                 fi
-                iptables -t nat -A DOCKER ${iptables_opt_src}! -i ${DOCKER_NET_INT} -p ${dst_proto} -m ${dst_proto} --dport ${src_port} -j DNAT --to-destination ${ipaddr}:${dst_port}
+                /sbin/iptables -t nat -A DOCKER ${iptables_opt_src}! -i ${DOCKER_NET_INT} -p ${dst_proto} -m ${dst_proto} --dport ${src_port} -j DNAT --to-destination ${ipaddr}:${dst_port}
             done
         fi
     done
@@ -117,5 +119,5 @@ iptables -A DOCKER-ISOLATION-STAGE-2 -j RETURN
 iptables -A DOCKER-USER -j RETURN
 
 if [ `iptables -t nat -nvL DOCKER | grep ${DOCKER_INT} | wc -l` -eq 0 ]; then
-    iptables -t nat -I DOCKER -i ${DOCKER_INT} -j RETURN
+    /sbin/iptables -t nat -I DOCKER -i ${DOCKER_INT} -j RETURN
 fi
